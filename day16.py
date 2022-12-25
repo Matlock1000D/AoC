f9e4c9a1f8b09d4bcd81f5117d5f6ab62789803b
lpjapp@gmail.com <lpjapp@gmail.com>
2022-12-25 15:46:16 +0200

    Päivä 16 osa 2 ei toimi

--- day16.py
+++ day16.py
@@ -7,6 +7,15 @@
 
 #133 on liian vähän
 
+class Pathobj:
+    def __init__(self, path, pressure, t) -> None:
+        self.path = path
+        self.pressure = pressure
+        self.t = t
+    
+    def current(self):
+        return self.path[-1]
+
 class Valve:
     def __init__(self, name:str, flowrate:int, linkedvalves:list) -> None:
         self.name = name
@@ -23,10 +32,35 @@ class Valve:
 def open_valve(valve:Valve):
     valve.open = True
 
+def find_distances(valvename, valves):  #ei kovin optimoitu tai nätti, mutta saa kelvata
+    goodvalves = []
+    for valve in valves.values():
+        if valve.flowrate > 0: goodvalves.append(valve.name)
+    distances = {}
+    for valve in goodvalves:
+        dist = 0
+        current = valvename
+        paths = [[valvename]]
+        found = False
+        while found == False:
+            newpaths = []
+            for path in paths:
+                if path[-1] == valve:
+                    found = True
+                    break
+                for linkedvalve in valves[path[-1]].linkedvalves:
+                    if linkedvalve not in path:
+                        newpath = path.copy()
+                        newpath.append(linkedvalve)
+                        newpaths.append(newpath)
+            if not found:
+                dist += 1
+                paths = newpaths
+        distances[valve] = dist
+    return distances
 
 def maximise_pressure(file, spec):
     valves = {}
-    #paths = []
     with open(file, 'r') as f:
         for line in f:
             if len(line) < 9: break
@@ -37,9 +71,8 @@ def maximise_pressure(file, spec):
             for i, value in enumerate(linkedvalves): linkedvalves[i] = value[:-1] #jämämerkit pois lopusta
             valves[name] = Valve(name, flowrate, linkedvalves)
 
-    #path = [('AA',0,[])]    #Nykytila, paine, avonaiset hanat
-    #paths.append(path)
-    max_t = 30
+    if spec == '1': max_t = 30
+    else: max_t = 26
     t = 0
     path = ['AA']
     opened = False
@@ -48,57 +81,166 @@ def maximise_pressure(file, spec):
     temp_maximum = 0
     timedir = 1 #1 = eteen, -1 = taakse
     dontgo = ''
-    while True:
-        if timedir == 1:
-            current = path[-1] 
-            if valves[current].flowrate > 0 and not valves[current].open and not dontopen:
-                valves[current].open_valve()
-                path.append(current)
-                opened = True
-                t += 1
-                temp_maximum += (max_t - t) * valves[current].flowrate
-            else:
-                try: last = path[-2]
-                except: last = []
-                if last == current: last = path[-3]
-                if dontgo != '': dontgo_index = valves[current].linkedvalves.index(dontgo)+1
-                else: dontgo_index = 0
-                foundway = False
-                for linkedvalve in valves[current].linkedvalves[dontgo_index:]:    #iteroidaan reittejä, kunnes löytyy sopiva
-                    if opened == False and linkedvalve == last: continue    #turha palata ellei ole avattu mitään
-                    #liikutaan seuraavaan paikkaan
-                    path.append(linkedvalve)
-                    opened = False
-                    dontopen = False
+    if spec == '1':
+        while True:
+            if timedir == 1:
+                current = path[-1] 
+                if valves[current].flowrate > 0 and not valves[current].open and not dontopen:
+                    valves[current].open_valve()
+                    path.append(current)
+                    opened = True
                     t += 1
-                    foundway = True
-                    dontgo = ''
-                    break
-                if not foundway: timedir = -1
-            if t >= max_t-1:
-                if maximum < temp_maximum: maximum = temp_maximum
-                timedir = -1
-        if timedir == -1:
-            current = path[-1]
-            if opened == True:
-                valves[current].close_valve()
-                path.pop()
-                opened = False
-                dontopen = True
-                temp_maximum -= (max_t - t) * valves[current].flowrate
-                t-=1
-                timedir = 1
-            else:
-                if t == 0: return maximum
-                path.pop()
-                if t > 1:
-                    if path[-1] == path[-2]: opened = True
+                    temp_maximum += (max_t - t) * valves[current].flowrate
+                else:
+                    try: last = path[-2]
+                    except: last = []
+                    if last == current: last = path[-3]
+                    if dontgo != '': dontgo_index = valves[current].linkedvalves.index(dontgo)+1
+                    else: dontgo_index = 0
+                    foundway = False
+                    for linkedvalve in valves[current].linkedvalves[dontgo_index:]:    #iteroidaan reittejä, kunnes löytyy sopiva
+                        if opened == False and linkedvalve == last: continue    #turha palata ellei ole avattu mitään
+                        #liikutaan seuraavaan paikkaan
+                        path.append(linkedvalve)
+                        opened = False
+                        dontopen = False
+                        t += 1
+                        foundway = True
+                        dontgo = ''
+                        break
+                    if not foundway: timedir = -1
+                if t >= max_t-1:
+                    if maximum < temp_maximum: maximum = temp_maximum
+                    timedir = -1
+            if timedir == -1:
+                current = path[-1]
+                if opened == True:
+                    valves[current].close_valve()
+                    path.pop()
+                    opened = False
+                    dontopen = True
+                    temp_maximum -= (max_t - t) * valves[current].flowrate
+                    t-=1
+                    timedir = 1
+                else:
+                    if t == 0: return maximum
+                    path.pop()
+                    if t > 1:
+                        if path[-1] == path[-2]: opened = True
+                        else: opened = False
                     else: opened = False
-                else: opened = False
-                dontgo = current
-                t-=1
-                timedir = 1
-                     
+                    dontgo = current
+                    t-=1
+                    timedir = 1
+    else:
+    #rakennetaan taulukko etäisyyksistä kelvollisille venttiileille
+        max_t = 26
+        maxpres = 0
+        goodvalves = {'AA':find_distances('AA', valves)}
+        for valve in valves.values():
+            if valve.flowrate > 0:
+                goodvalves[valve.name] = find_distances(valve.name, valves)
+        valves['AA'].open_valve()   #avataan, niin algoritmi ei yritä tänne
+        paths = [Pathobj(['AA'],0,0)]
+        while True:
+            newpathobjs = []
+            for pathobj in paths:
+                for nextvalve in goodvalves[pathobj.current()]:
+                    time_remaining = max_t - pathobj.t - 1
+                    if (not valves[nextvalve].open) and goodvalves[pathobj.current()][nextvalve] < time_remaining and nextvalve not in pathobj.path:
+                        newpath = pathobj.path.copy()
+                        newpath.append(nextvalve)
+                        t = pathobj.t+goodvalves[pathobj.current()][nextvalve] + 1
+                        pressure = pathobj.pressure + (max_t-t) * valves[nextvalve].flowrate
+                        newpathobjs.append(Pathobj(newpath,pressure,t))
+            if len(newpathobjs) == 0: break
+            else: paths = newpathobjs.copy()
+        for pather in paths:
+            for valve in goodvalves:
+                valves[valve].close_valve()
+            for valve in pather.path:
+                valves[valve].open_valve()
+            elepaths = [Pathobj(['AA'],pather.pressure,0)]
+            while True:
+                newpathobjs = []
+                for pathobj in elepaths:
+                    for nextvalve in goodvalves[pathobj.current()]:
+                        time_remaining = max_t - pathobj.t - 1
+                        if (not valves[nextvalve].open) and goodvalves[pathobj.current()][nextvalve] < time_remaining and nextvalve not in pathobj.path:
+                            newpath = pathobj.path.copy()
+                            newpath.append(nextvalve)
+                            t = pathobj.t+goodvalves[pathobj.current()][nextvalve] + 1
+                            pressure = pathobj.pressure + (max_t-t) * valves[nextvalve].flowrate
+                            newpathobjs.append(Pathobj(newpath,pressure,t))
+                if len(newpathobjs) == 0: break #elepathsit tälle pathille
+                else: elepaths = newpathobjs.copy()
+            tempmax = max(x.pressure for x in elepaths)
+            if tempmax > maxpres: maxpres = tempmax
+        return maxpres
+
+#2397
+#2409
+#2562
+
+    """Liian hidas tämäkin
+    else:
+        path = ['AA','AA']
+        dontopen = [False,False]
+        opened = [False,False]
+        dontgo = ['','']
+        while True:
+            if timedir == 1:
+                current = path[-2] 
+                if valves[current].flowrate > 0 and not valves[current].open and not dontopen[t%2]:
+                    valves[current].open_valve()
+                    path.append(current)
+                    opened[t%2] = True
+                    t += 1
+                    temp_maximum += (max_t - t)//2 * valves[current].flowrate
+                else:
+                    try: last = path[-3]
+                    except: last = []
+                    if t == 1: last == []
+                    elif last == current: last = path[-5]
+                    if dontgo[t%2] != '': dontgo_index = valves[current].linkedvalves.index(dontgo[t%2])+1
+                    else: dontgo_index = 0
+                    foundway = False
+                    for linkedvalve in valves[current].linkedvalves[dontgo_index:]:    #iteroidaan reittejä, kunnes löytyy sopiva
+                        if opened[t%2] == False and linkedvalve == last: continue    #turha palata ellei ole avattu mitään
+                        #liikutaan seuraavaan paikkaan
+                        path.append(linkedvalve)
+                        opened[t%2] = False
+                        dontopen[t%2] = False
+                        dontgo[t%2] = ''
+                        t += 1
+                        foundway = True
+                        break
+                    if not foundway: timedir = -1
+                if t >= max_t-2 or sum(1 for x in valves.values() if x.open) >= goodvalves:    #lopussa, jos aika on loppu tai kaikki venttiilit ovat auki
+                    if maximum < temp_maximum: maximum = temp_maximum
+                    timedir = -1
+            if timedir == -1:
+                current = path[-1]
+                if opened[t%2] == True:
+                    valves[current].close_valve()
+                    opened[t%2] = False
+                    dontopen[t%2] = True
+                    path.pop()
+                    temp_maximum -= (max_t - t)//2 * valves[current].flowrate
+                    t-=1
+                    timedir = 1
+                else:
+                    if t == 0: return maximum
+                    path.pop()
+                    if t > 2:
+                        if path[-1] == path[-3]: opened[t%2] = True
+                        else: opened[t%2] = False
+                    else: opened[t%2] = False
+                    dontgo[t%2] = current
+                    t-=1
+                    timedir = 1
+    """
+
     """Liian hidasta
     for t in range(max_t):
         newpaths = []
