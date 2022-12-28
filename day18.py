c1ba32f6f9bf9f731fd38c6ed88c1e8c55ccef75
lpjapp@gmail.com <lpjapp@gmail.com>
2022-12-28 14:09:29 +0200

    Päivä 18 osa 2

--- day18.py
+++ day18.py
@@ -18,15 +18,15 @@ def count_sides(block):
                         co_index[i] += x
                         if not block[tuple(co_index)]: sides += 1
     sides = 0
-    for x in range(50):
-        for y in range(50):
-            for z in range(50):
+    for x in range(30):
+        for y in range(30):
+            for z in range(30):
                 if block[x,y,z]:
                     for i in range(3):
                         for j in [-1,1]:
                             co = [x,y,z]
                             co[i] = co[i]+j
-                            if co[i] < 0 or co[i] >= 50:
+                            if co[i] < 0 or co[i] >= 30:
                                 sides += 1
                                 continue
                             else:
@@ -34,16 +34,42 @@ def count_sides(block):
 
     return sides
 
+def fill_insides(block):
+    emptiness = set()
+    emptiness.add((0,0,0))
+    newstep = [(0,0,0)]
+    while len(newstep) > 0:
+        laststep = newstep.copy()
+        newstep = set()
+        for index in laststep:
+            for i in range(3):
+                for j in [-1, 1]:
+                    newindex = list(index)
+                    newindex[i] += j
+                    newindex = tuple(newindex)
+                    if newindex[i] >= 0 and newindex[i]<30:
+                        if not block[newindex] and newindex not in emptiness:
+                            newstep.add(newindex)
+        emptiness.update(newstep)
+    #täytetään sisäosat
+    with np.nditer(block, flags=['multi_index'], op_flags=['readwrite']) as it:
+        for x in it:
+            if not x:
+                if it.multi_index not in emptiness:
+                    x[...] = True
+                        
+
 
 
 #Tänne voisi syöttää myös luetun listan, niin ei tarvitse käydä koko matriisia läpi.
 
 def get_sides(file, spec):
-    block = np.zeros((50,50,50),dtype=bool)
+    block = np.zeros((30,30,30),dtype=bool)
     with open(file, 'r') as f:
         for line in f:
             if len(line) < 1: break
             readline = line.strip().split(',')
+            #Tämä rikkoo jotenkin indeksoinnin, ei jaksa debugata. Tehdään ennemmin ”tarpeeksi iso” matriisi
             """
             for i, co in enumerate(readline):
                 co = int(co)
@@ -53,5 +79,10 @@ def get_sides(file, spec):
                     newshape = tuple(newshape)
                     np.resize(block, newshape)
             """
-            block[int(readline[0]),int(readline[1]),int(readline[2])] = True
+            if spec == '1':
+                block[int(readline[0]),int(readline[1]),int(readline[2])] = True
+            else:
+                block[int(readline[0])+1,int(readline[1])+1,int(readline[2])+1] = True
+    if spec == '2':
+        fill_insides(block)
     return count_sides(block)
